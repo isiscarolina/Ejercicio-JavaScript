80d833fdc4662f9442e6be2bb7a14bf248d5ccc1
isis parra <isiscapano@gmail.com>
2020-07-22 14:17:27 -0300

    ejercicio Java Script

---
+++ ejercicio.js
@@ -0,0 +1,23 @@
+let nome= prompt ("coloque seu nome")
+let sobrenome= prompt( "coloque seu sobrenome")
+let idade = prompt( "coloque sua idade")
+let numero= Number(idade)
+
+function resultado() {
+    console.log("Olá " + nome + " " + sobrenome + ", " + "sua idade é: " + numero)
+}
+
+resultado();
+
+
+
+let nome1= prompt ("coloque seu nome")
+let sobrenome1= prompt ("coloque seu sobrenome")
+let idade1= prompt ("coloque sua idade")
+
+
+
+console.table ([nome1, sobrenome1,idade1])
+
+    
+
